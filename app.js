a797dfc9eb83a315a37a24007f82825273f0a08e
ahmedjadan <a.jadan7@gmail.com>
2020-04-22 01:03:06 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.modal').modal();
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.tabs').tabs();
+    $('.datepicker').datepicker({
+       disableWeekends: true
+    });
+    $('.tooltipped').tooltip();
+    // $('.scrollspy').scrollSpy();
+
+})
+
